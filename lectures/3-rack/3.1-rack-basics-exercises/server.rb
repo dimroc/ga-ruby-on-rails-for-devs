4ef10612d35d0824ed8f2df55a50c7ecfb8ed69a
dblock <dblock@dblock.org>
2012-01-12 09:29:00 -0500

    Minor layout.

--- lectures/3-rack/3.1-rack-basics-exercises/server.rb
+++ lectures/3-rack/3.1-rack-basics-exercises/server.rb
@@ -4,10 +4,10 @@ Implement a basic web server with Rack.
 
 1. Server should be able to serve files from a `public` directory with `index.html` as the default file name. For example, using a browser one should be able to navigate to http://localhost:9292 or http://localhost:9292/index.html and see the contents of `index.html`.
 
-|-Gemfile
-|-server.rb
-|+public
-    |-index.html
+    |-Gemfile
+    |-server.rb
+    |+public
+        |-index.html
     
 2. Server should return the correct `Content-Length` header. The value of the Content-Length header is a string that contains the number of bytes that are sent from the server to the client.
 

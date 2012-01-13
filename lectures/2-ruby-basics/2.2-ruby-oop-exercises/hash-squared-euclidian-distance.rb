58e29b649b691188a5fccb34ad9eb258bc9dbe27
dblock <dblock@dblock.org>
2012-01-13 08:15:43 -0500

    Better implementation and a check.

--- lectures/2-ruby-basics/2.2-ruby-oop-exercises/hash-squared-euclidian-distance.rb
+++ lectures/2-ruby-basics/2.2-ruby-oop-exercises/hash-squared-euclidian-distance.rb
@@ -2,10 +2,12 @@
 
 Add a `squared_euclidean_distance` method to the `Hash` class that takes another `Hash` as a parameter and calculates the euclidean distance with another Hash. A Euclidean distance between `{ :x => 1, :y => 2 }` and `{ :x => 3, :y => 4 }` is `(1 - 3)^2 + (2 - 4)^2`.
 
-    puts ( {}.squared_euclidean_distance({}) ) # 0
-    puts ( { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :x => 5 }) ) # 10
-    puts ( { :x => 10, :y => 20 }.squared_euclidean_distance({}) ) # 500
-    puts ( {}.squared_euclidean_distance({ :x => 10, :y => 20 }) ) # 500
+    {}.squared_euclidean_distance({}) # 0
+    { :x => 1, :y => 2 }.squared_euclidean_distance({ :z => 3 }) # 14
+    { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :z => 5 }) # 30
+    { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :x => 5 }) # 10
+    { :x => 10, :y => 20 }.squared_euclidean_distance({}) # 500
+    {}.squared_euclidean_distance({ :x => 10, :y => 20 }) # 500
 
 =end
 
@@ -21,8 +23,11 @@ class Hash
 
 end
 
-puts ( {}.squared_euclidean_distance({}) ) # 0
-puts ( { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :x => 5 }) ) # 10
-puts ( { :x => 10, :y => 20 }.squared_euclidean_distance({}) ) # 500
-puts ( {}.squared_euclidean_distance({ :x => 10, :y => 20 }) ) # 500
+raise "invalid distance between two different hashes" unless 14 == ( { :x => 1, :y => 2 }.squared_euclidean_distance({ :z => 3 }) )
+raise "invalid distance between empty hashes" unless 0 == ( {}.squared_euclidean_distance({}) )
+raise "invalid distance between two overlapping hashes" unless 10 == ( { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :x => 5 }) )
+raise "invalid distance between two partially overlapping hashes" unless 30 == ( { :x => 2, :y => 3 }.squared_euclidean_distance({ :y => 4, :z => 5 }) )
+raise "invalid distance between a hash and an empty hash" unless 500 == ( { :x => 10, :y => 20 }.squared_euclidean_distance({}) )
+raise "invalid distance between an empty hash and a hash" unless 500 == ( {}.squared_euclidean_distance({ :x => 10, :y => 20 }) )
+puts "correct results in squared_euclidean_distance"
 

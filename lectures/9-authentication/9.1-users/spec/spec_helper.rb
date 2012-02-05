0dbe9fc953628213b2ff3666f522c47c06aa1e89
dblock <dblock@dblock.org>
2012-02-05 15:07:17 -0500

    Working RSpec.

--- lectures/9-authentication/9.1-users/spec/spec_helper.rb
+++ lectures/9-authentication/9.1-users/spec/spec_helper.rb
@@ -13,9 +13,6 @@ Spork.prefork do
   RSpec.configure do |config|
     config.mock_with :rspec
     config.expect_with :rspec
-    config.after(:all) do
-      puts "All tests have finished."
-    end
   end
 end
 

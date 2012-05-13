9243c5ab5d2b4aeb39919a8212d4f7ee917a143d
Dimitri Roche <dimroc@gmail.com>
2012-05-13 13:40:07 -0400

    5.2) Move sample rails project to 5.2-bdd-demo-complete

--- lectures/05-rspec/5.2-bdd-demo-complete/spec/controllers/posts_controller_spec.rb
+++ lectures/05-rspec/5.2-bdd-demo-complete/spec/controllers/posts_controller_spec.rb
@@ -16,6 +16,15 @@ describe PostsController do
       assigns(:posts).should_not be_nil
     end
 
+    it "POST#create" do
+      title = Faker::Lorem.word
+      content = Faker::Lorem.paragraph
+      expect {
+        post :create, post: { title: title, content: content }
+        assigns(:post).should_not be_nil
+      }.to change { Post.count }.by(1)
+    end
+
     it "GET#new" do
       get :new
       post = assigns(:post)
